f5c50f7b48fac70266275d5f4333420bca8a0fe1
Alfredo <xbox.07@hotmail.com>
2020-05-29 08:22:15 -0500

    Iniciando proyecto, tema property Binding

---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent {
+
+  nombrePersona = 'Juan';
+  apellidoPersona = 'Perez';
+  edad = 28;
+
+}
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personas',
+  templateUrl: './personas.component.html',
+  // styleUrls: ['./personas.component.css']
+  styles: [`
+    h1{
+      color:blue;
+    }
+    `]
+})
+export class PersonasComponent {
+
+  agregarPersona = false;
+
+
+}
